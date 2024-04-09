51afa25ea78f1f4ffc72d8c89fbc5f6c73554796
Quang Huy <nguyenquanghuy172000@gmail.com>
2024-04-09 16:02:17 +0700

    fe/add_count_down

--- app/(platform)/program/_components/information/leftinformation/leftinformation.tsx
+++ app/(platform)/program/_components/information/leftinformation/leftinformation.tsx
@@ -3,9 +3,13 @@ import Time from '../time'
 import DetailInformation from '../detailinformation'
 
 function LeftInformation() {
+  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+  const NOW_IN_MS = new Date().getTime();
+
+  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
   return (
     <div>
-        <Time/>
+        <Time targetDate={dateTimeAfterThreeDays}/>
         <DetailInformation/>
     </div>
   )
